2719ff6564a526820407c14d82b16414925499d6
Esty Segal <esty9089777@example.com>
2026-03-22 13:37:26 +0200

    Add IContext and IRepository interfaces for DB and CRUD

---
+++ Service/Services/UserService.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Common.Dto;
+using Repository.Entities;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IMapper _mapper;
+        public Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Add(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(int id, User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public UserDto Register(RegisterDto register)
+        {
+            throw new NotImplementedException();
+        }
+        public UserDto Login(LoginDto login)
+        {
+            throw new NotImplementedException();
+        }
+        public UserDto UpdateProfile(int userId, UpdateUserDto updateUserDto)
+        {
+            throw new NotImplementedException();
+        }
+        public void ChangePassword(int userId, ChangePasswordDto changePasswordDto)
+        {
+            throw new NotImplementedException();
+        }
+        public void ResetPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddAttraction(int userId, BranchDto branchDto)
+        {
+            throw new NotImplementedException();
+        }
+        public void AddRoute(int userId, RouteDto routeDto)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveAtraction(int userId, int branchId)
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveRoute(int userId, int routeId)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+        public TripDto GetTrip(int userId, int tripId)
+        {
+            throw new NotImplementedException();
+        }
+        public List<TripDto> GetUserTrips(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
