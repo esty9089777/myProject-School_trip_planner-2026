38b4e64a24725eec789f039f63b826dd935369e2
Esty Segal <esty9089777@example.com>
2026-03-22 12:41:47 +0200

    Added IAttractionService

--- Service/Interfaces/IAttractionService.cs
+++ Service/Interfaces/IAttractionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace Service.Interfaces
 {
-    internal interface IAttractionService
+    public interface IAttractionService
     {
+        //attractions crud
+        AttractionDto AddAttraction(AttractionDto attractionDto);
+        void UpdateAttraction(int attractionId, AttractionDto attractionDto);
+        void DeleteAttraction(int attractionId);
+        AttractionDto GetAttractionById(int attractionId);
+
+        //branch attractions crud
+        BranchDto AddBranch(int attractionId, BranchDto branchDto);
+        void UpdateBranch(int branchId, BranchDto branchDto);
+        void DeleteBranch(int branchId);
+        BranchDto GetBranchById(int branchId);
+        List<BranchDto> GetBranchesByAttractionId(int attractionId);
     }
 }
--- Service/Interfaces/IRouteService.cs
+++ Service/Interfaces/IRouteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Service.Interfaces
 {
-    internal interface IRouteService
+    public interface IRouteService
     {
+        void AddRoute(RouteDto routeDto);
+        void UpdateRoute(int routeId, RouteDto routeDto);
+        void DeleteRoute(int routeId);
+        RouteDto GetRouteById(int routeId);
     }
 }
