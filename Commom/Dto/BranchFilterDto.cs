08f21a92df9f938c17c3e629cbd14b7deb527d1d
Esty Segal <esty9089777@example.com>
2026-04-18 23:15:43 +0300

    add BranchFilterDto

--- Commom/Dto/BranchFilterDto.cs
+++ Commom/Dto/BranchFilterDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Common.Dto
 {
-    internal class BranchFilterDto
+    public class BranchFilterDto
     {
+        public bool? BranchName { get; set; }
+        public Direction? Direction { get; set; }
+        public AgeGroupEnum? AgeGroup { get; set; }
+        public bool? IsFree { get; set; }
+        public bool? IsWet { get; set; }
+        public AttractionCategoryEnum? Category { get; set; }
     }
 }
--- Service/Interfaces/IBranchService.cs
+++ Service/Interfaces/IBranchService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Service.Interfaces
 {
-    internal interface IBranchService
+    public interface IBranchService
     {
+        BranchDto GetAllBranches();
+        BranchDto GetBranchById(int id);
+        BranchDto AddBranch(CreateBranchDto dto);
+        void UpdateBranch(int id, UpdateBranchDto dto);
+        void DeleteBranch(int id);
+        BranchDto FilterBranches(BranchFilterDto filter);
+        BranchDto GetNearbyBranches(double lat, double lng);
     }
 }
