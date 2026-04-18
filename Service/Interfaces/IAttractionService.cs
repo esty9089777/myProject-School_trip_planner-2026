1416a6b2fa47f0e85cea2b21841cfb1ca7432ed4
Esty Segal <esty9089777@example.com>
2026-04-18 23:39:13 +0300

    update IBrancAttractionService

--- Service/Interfaces/IAttractionService.cs
+++ Service/Interfaces/IAttractionService.cs
@@ -10,17 +10,9 @@ namespace Service.Interfaces
 {
     public interface IAttractionService:IService<Attraction>
     {
-        //attractions crud
         AttractionDto AddAttraction(AttractionDto attractionDto);
         void UpdateAttraction(int attractionId, AttractionDto attractionDto);
         void DeleteAttraction(int attractionId);
         AttractionDto GetAttractionById(int attractionId);
-
-        //branch attractions crud
-        BranchDto AddBranch(int attractionId, BranchDto branchDto);
-        void UpdateBranch(int branchId, BranchDto branchDto);
-        void DeleteBranch(int branchId);
-        BranchDto GetBranchById(int branchId);
-        List<BranchDto> GetBranchesByAttractionId(int attractionId);
     }
 }
