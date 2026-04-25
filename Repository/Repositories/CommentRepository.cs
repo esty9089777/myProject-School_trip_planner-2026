81ed4dd41d3c1a787d79a03308a37f98e5ea2ed4
Esty Segal <esty9089777@example.com>
2026-04-26 02:55:13 +0300

    Implement CommentRepository with AutoMapper and standardized field naming

--- Repository/Repositories/CommentRepository.cs
+++ Repository/Repositories/CommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using myProjectTrips.Interfaces;
 using Repository.Entities;
 using Repository.Interfaces;
@@ -12,33 +13,35 @@ namespace Repository.Repositories
 {
     public class CommentRepository : ICommentRepository
     {
-        private readonly IContext ctx;
-        public CommentRepository(IContext context)
+        private readonly IContext _ctx;
+        private readonly IMapper _mapper;
+        public CommentRepository(IContext context, IMapper mapper)
         {
-            ctx = context;
+            _ctx = context;
+            _mapper = mapper;
         }
 
         public async Task<Comment> Add(Comment item)
         {
-            await ctx.Comments.AddAsync(item);
-            await ctx.Save();
+            await _ctx.Comments.AddAsync(item);
+            await _ctx.Save();
             return item;
         }
 
         public async Task Delete(int id)
         {
-            var c = ctx.Comments.FirstOrDefault(x => x.CommentId == id);
+            var c = await _ctx.Comments.FirstOrDefaultAsync(x => x.CommentId == id);
             if (c == null)
             {
                 throw new Exception("Comment not found");
             }
-            ctx.Comments.Remove(c);
-            await ctx.Save();
+            _ctx.Comments.Remove(c);
+            await _ctx.Save();
         }
 
         public async Task<List<Comment>> GetAll()
         {
-            return await ctx.Comments.ToListAsync();
+            return await _ctx.Comments.ToListAsync();
         }
 
         public async Task<Comment> GetByEmail(string email)
@@ -48,40 +51,34 @@ namespace Repository.Repositories
 
         public Task<Comment> GetById(int id)
         {
-            return ctx.Comments.FirstOrDefaultAsync(x => x.CommentId == id);
+            return _ctx.Comments.FirstOrDefaultAsync(x => x.CommentId == id);
         }
 
         public async Task<List<Comment>> GetCommentByBranchId(int branchId)
         {
-            return await ctx.Comments
+            return await _ctx.Comments
                     .Where(x => x.BranchId == branchId)
                     .ToListAsync();
         }
 
         public async Task<List<Comment>> GetCommentByRouteId(int routeId)
         {
-            return await ctx.Comments
+            return await _ctx.Comments
                     .Where(x => x.RouteId == routeId)
                     .ToListAsync();
         }
 
         public async Task<Comment> Update(int id, Comment item)
         {
-            var c = await ctx.Comments.FirstOrDefaultAsync(x => x.CommentId == id);
-            if (c == null)
+            var existingComment = await _ctx.Comments.FirstOrDefaultAsync(a => a.CommentId == id);
+            if (existingComment == null)
             {
                 throw new Exception("Comment not found");
             }
-            c.SchoolName = item.SchoolName;
-            c.CommentId = item.CommentId;
-            c.BranchId = item.BranchId;
-            c.RouteId = item.RouteId;
-            c.myComment = item.myComment;
-            c.DateCommon = item.DateCommon;
-            c.Branch = item.Branch;
-            c.Route = item.Route;
-            await ctx.Save();
-            return c;
+
+            _mapper.Map(item, existingComment);
+            await _ctx.Save();
+            return existingComment;
         }
     }
 }
