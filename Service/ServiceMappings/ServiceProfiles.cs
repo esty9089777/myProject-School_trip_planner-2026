36e0892f6d4c72ed395a09fc2e45c386cadb62ad
Esty Segal <esty9089777@example.com>
2026-04-27 00:37:08 +0300

    implement AutoMapper profiles for Service layer

---
+++ Service/ServiceMappings/ServiceProfiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Common.Dto;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Mappings
+{
+    public class ServiceProfiles : Profile
+    {
+        public ServiceProfiles()
+        {
+            CreateMap<Attraction, AttractionDto>().ReverseMap();
+            CreateMap<Availability, AvailabilityDto>().ReverseMap();
+            CreateMap<Branch, BranchDto>().ReverseMap();
+            CreateMap<Comment, CommentDto>().ReverseMap();
+            CreateMap<Route, RouteDto>().ReverseMap();
+            CreateMap<Trip, TripDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+
+            CreateMap<RegisterDto, User>();
+            CreateMap<UpdateUserDto, User>();
+            CreateMap<ChangePasswordDto, User>();
+
+            CreateMap<User, ResetPasswordDto>();
+        }
+    }
+}
