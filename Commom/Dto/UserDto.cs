df883c25e41a3f611cee323cb23977896686da48
Esty Segal <esty9089777@example.com>
2026-04-28 22:36:26 +0300

    refactor(user-dto): add UserId to UserDto for consistent entity identification

--- Commom/Dto/UserDto.cs
+++ Commom/Dto/UserDto.cs
@@ -9,6 +9,7 @@ namespace Common.Dto
 {
     public class UserDto
     {
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string UserPhoneNumber { get; set; }
