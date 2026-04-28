db7b750f3b065233ecf11bcf35a9a68a105b1695
Esty Segal <esty9089777@example.com>
2026-04-28 20:03:02 +0300

    feat: implement core user management and authentication logic

--- Service/Services/UserService.cs
+++ Service/Services/UserService.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    public class UserService:IUserService
+    public class UserService : IUserService
     {
         private readonly IRepository<User> _repository;
         private readonly IMapper _mapper;
@@ -21,69 +21,76 @@ namespace Service.Services
             _repository = repository;
             _mapper = mapper;
         }
-        public async Task<List<UserDto>> GetAll()
+
+        //public async Task<UserDto> Register(RegisterDto register)
+        //{
+        //    var user = await _repository.GetByUsername(register.UserName);
+        //    if (user != null)
+        //    {
+        //        throw new Exception("User already exists");
+        //    }
+        //    var userEntity = _mapper.Map<User>(register);
+        //    var addedUser = await _repository.Add(userEntity);
+
+        //    return _mapper.Map<UserDto>(addedUser);
+        //}
+
+
+        //public async Task AddAttraction(UserDto userDto, AttractionDto dto)
+        //{
+        //    if (userDto == null)
+        //    {
+        //        throw new Exception("User not found");
+        //    }
+        //    var attraction = _mapper.Map<Attraction>(dto);
+        //    var existingAttraction = await _repository.GetAttractionById(attraction.AttractionId);
+        //    var user = _mapper.Map<User>(userDto);
+
+        //    if (existingAttraction == null)
+        //    {
+        //        await _repository.AddAttraction(attraction, user);
+        //    }
+        //    var branch = new Branch
+        //    {
+        //        BranchName = attraction.AttraName,
+
+        //    };
+        //    await _repository.AddBranch(branch);
+        //}
+        //public void AddRoute(int userId, RouteDto routeDto)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public async Task RemoveAttraction(int userId, int attractionId)
+        //{
+        //    Attraction attraction = await _repository.GetAttractionById(attractionId);
+
+        //    if (attraction == null)
+        //    {
+        //        throw new Exception("Attraction not found");
+        //    }
+        //    if (userId != attraction.CreatorId)
+        //    {
+        //        throw new Exception("User is not the creator of the attraction");
+        //    }
+        //    await _repository.RemoveAttraction(attraction, userId);
+        //}
+
+        public Task<UserDto> Add(UserDto item)
         {
-            var user = await _repository.GetAll();
-            var userDtos = _mapper.Map<List<UserDto>>(user);
-            return userDtos;
+            throw new NotImplementedException();
         }
 
-        public async Task<UserDto> GetById(int id)
+        public Task AddAttraction(int userId, AttractionDto branchDto)
         {
-            var user = await _repository.GetById(id);
-            var userDto = _mapper.Map<UserDto>(user);
-            return userDto;
+            throw new NotImplementedException();
         }
 
-        public async Task<UserDto> Add(UserDto item)
+        public Task AddRoute(int userId, RouteDto routeDto)
         {
-            var userEntity = _mapper.Map<User>(item);
-            var addedUser = await _repository.Add(userEntity);
-            return _mapper.Map<UserDto>(addedUser);
+            throw new NotImplementedException();
         }
 
-        public async Task<UserDto> Update(int id, UserDto item)
-        {
-            var userEntity = _mapper.Map<User>(item);
-            var updatedUser = await _repository.Update(id, userEntity);
-            return _mapper.Map<UserDto>(updatedUser);
-        }
-
-        public async Task Delete(int id)
-        {
-            await _repository.Delete(id);
-        }
-
-        public async Task<UserDto> Register(RegisterDto register)
-        {
-            var user = await _repository.GetByUsername(register.UserName);
-            if (user != null)
-            {
-                throw new Exception("User already exists");
-            }
-            var userEntity = _mapper.Map<User>(register);
-            var addedUser = await _repository.Add(userEntity);
-
-            return _mapper.Map<UserDto>(addedUser);
-        }
-
-        public async UserDto Login(LoginDto login)
-        {
-            var user = await _repository.GetByEmail(login.Email);
-            if (user == null)
-                throw new Exception("User not found");
-
-            if (user.Password != login.Password)
-                throw new Exception("Wrong password");
-
-            return _mapper.Map<UserDto>(user);
-        }
-        public async Task<UserDto> UpdateProfile(int userId, UpdateUserDto updateUserDto)
-        {
-            var user = _mapper.Map<User>(updateUserDto);
-            user.UserId = userId;
-            return await _repository.Update(user);
-        }
         public async Task ChangePassword(int userId, ChangePasswordDto changePasswordDto)
         {
             var user = await _repository.GetById(userId);
@@ -104,136 +111,78 @@ namespace Service.Services
                 throw new Exception("New password and confirmation do not match");
             }
             user.UserPassword = changePasswordDto.NewPassword;
-            await _repository.Update(user);
+            await _repository.Update(user.UserId, user);
         }
-        public async Task ResetPassword(ResetPasswordDto dto)
+
+        public Task Delete(int id)
         {
-            var user = await _repository.GetByEmail(dto.Email);
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TripDto> GetTrip(int userId, int tripId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TripDto>> GetUserTrips(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDto> Login(LoginDto login)
+        {
+            var user = await _repository.GetByEmail(login.Email);
+            if (user == null)
+                throw new Exception("User not found");
+
+            if (user.UserPassword != login.Password)
+                throw new Exception("Wrong password");
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public Task RemoveAttraction(int userId, int attractionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRoute(int userId, int routeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ResetPassword(string email)
+        {
+            var user = await _repository.GetByEmail(email);
             if (user == null)
             {
                 throw new Exception("User not found");
             }
-            if (dto.NewPassword != dto.ConfirmNewPassword)
+            user.UserPassword = "AB123#c63*";
+            await _repository.Update(user.UserId, user);
+        }
+
+        public async Task<UserDto> Update(int id, UserDto item)
+        {
+            var existingUser = await _repository.GetById(id);
+            if (existingUser == null)
             {
-                throw new Exception("New password and confirmation do not match");
+                throw new KeyNotFoundException($"User with id {id} not found.");
             }
-            user.UserPassword = dto.NewPassword;
-            await _repository.Update(user);
-        }
-        public async Task AddAttraction(UserDto userDto, AttractionDto dto)
-        {
-            if (userDto == null)
-            {
-                throw new Exception("User not found");
-            }
-            var attraction = _mapper.Map<Attraction>(dto);
-            var existingAttraction = await _repository.GetAttractionById(attraction.AttractionId);
-            var user = _mapper.Map<User>(userDto);
-
-            if (existingAttraction == null)
-            {
-                await _repository.AddAttraction(attraction, user);
-            }
-            var branch = new Branch
-            {
-                BranchName = attraction.AttraName,
-                
-            };
-            await _repository.AddBranch(branch);
-        }
-        public void AddRoute(int userId, RouteDto routeDto)
-        {
-            throw new NotImplementedException();
-        }
-        public async Task RemoveAttraction(int userId, int attractionId)
-        {
-            Attraction attraction = await _repository.GetAttractionById(attractionId);
-
-            if (attraction == null)
-            {
-                throw new Exception("Attraction not found");
-            }
-            if (userId != attraction.CreatorId)
-            {
-                throw new Exception("User is not the creator of the attraction");
-            }
-            await _repository.RemoveAttraction(attraction, userId);
-        }
-        public void RemoveRoute(int userId, int routeId)
-        {
-            throw new NotImplementedException();
-        }
-        public void DeleteUser(int userId)
-        {
-            throw new NotImplementedException();
-        }
-        public TripDto GetTrip(int userId, int tripId)
-        {
-            throw new NotImplementedException();
-        }
-        public List<TripDto> GetUserTrips(int userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<List<User>> IService<User>.GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<User> IService<User>.GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        UserDto IUserService.Register(RegisterDto register)
-        {
-            throw new NotImplementedException();
-        }
-
-        UserDto IUserService.UpdateProfile(int userId, UpdateUserDto updateUserDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ResetPassword(string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task AddAttraction(int userId, BranchDto branchDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserService.AddRoute(int userId, RouteDto routeDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserService.RemoveAtraction(int userId, int branchId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserService.RemoveRoute(int userId, int routeId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task IUserService.DeleteUser(int userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<User> Add(User item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<User> Update(int id, User item)
-        {
-            throw new NotImplementedException();
+            var userToUpdate = _mapper.Map<User>(item);
+            userToUpdate.UserId = id; 
+            var updatedUser = await _repository.Update(id, userToUpdate);
+            return _mapper.Map<UserDto>(updatedUser);
         }
     }
 }
