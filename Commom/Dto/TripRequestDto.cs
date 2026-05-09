d65d4eac3200a0fca74be2861df99b3f41b5dddc
Esty Segal <esty9089777@example.com>
2026-05-10 01:52:00 +0300

    Add TripRequestDto class for trip request data
    Introduced TripRequestDto in the Common.Dto namespace to encapsulate trip request details, including trip date, duration, direction, budget, wetness, difficulty level, and notes. This DTO will facilitate structured data transfer for trip-related operations.

---
+++ Commom/Dto/TripRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Dto
+{
+    public class TripRequestDto
+    {
+        public DateTime TripDate { get; set; }
+        public int DurationInHours { get; set; }
+        public Direction direction { get; set; }
+        public decimal MaxBudgetPerStudent { get; set; }
+        public bool IsWet { get; set; }
+        public string DifficultyLevel { get; set; }
+        public string FreeTextNotes { get; set; }
+    }
+}
